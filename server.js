522663ca5b3386941cd26f6fb0b68be637796542
Maurice-K <maurice.i.kelly@gmail.com>
2020-10-04 16:57:45 -0500

    Added server.js for deployment and changed environment files to hold API keys

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express()
+
+
+app.use(express.static('./dist/angular'));
+
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/angular/'}
+  );
+});
+
+app.listen(process.env.PORT || 8080);
